83d8e9a3a964f7eb860ea81852c3f5ba3ea5c686
jorge vergara <jorgevergarasou@gmail.com>
2025-10-14 11:37:52 -0300

    Updates

---
+++ assets/User-CsAgcS-7.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DIsqzmEn.js";function i(){return e.jsxs("div",{children:[e.jsx("h1",{children:"Bienvenido "}),e.jsx("p",{children:"Correo: "}),e.jsx("p",{children:"Rol: "})]})}export{i as default};
